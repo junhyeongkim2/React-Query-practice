9cc107df3030577fe08dd2b92e9c09dbef28bda3
jun <ggprgrkjh@naver.com>
2022-10-14 02:02:43 +0900

    feat : infinite-swapi

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
+import { Query, QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider clinet={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
   );
 }
 
